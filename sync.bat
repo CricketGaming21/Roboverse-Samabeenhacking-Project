cc82c16af2d6b254572f590a4cceaca1b505cf56
CricketGaming21 <markiemoo1234@gmail.com>
2026-06-06 17:44:41 +0800

    chore: remove test files

--- sync.bat
+++ sync.bat
@@ -1,9 +1,8 @@
 @echo off
 cd /d C:\Users\markk\Desktop\px4-finals
-echo Syncing to GitHub...
 git add -A
-git commit -m "sync: windows update %date% %time%"
+git commit -m "sync: windows update"
 git push origin finals
 echo.
-echo Done! Changes pushed to GitHub.
+echo Done!
 pause
